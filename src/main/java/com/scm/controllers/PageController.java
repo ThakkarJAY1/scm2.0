83a8274e34adce596f990fe3f65e2492a1551839
Jay <jaythakkar618@gmail.com>
2024-05-23 14:54:28 -0400

    first commit

---
+++ src/main/java/com/scm/controllers/PageController.java
@@ -0,0 +1,34 @@
+package com.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+    
+    @RequestMapping("/home")
+    public String home(Model model){ 
+        System.out.println("Home Page Handler");
+        model.addAttribute("name", "Substring Technologies");
+        model.addAttribute("youtubeChannel", "Learn Code with durgesh");
+        return "home";
+    }
+    // about route
+    @RequestMapping("/about")
+    public String aboutPage(Model model){
+        model.addAttribute("isLogin", true);
+        System.out.println("About Page Loading");
+        return "about";
+    }
+
+    //services 
+    @RequestMapping("/services")
+    public String servicesPage(){
+        System.out.println("Services Page Loading");
+        return "services";
+    }
+
+
+
+}
